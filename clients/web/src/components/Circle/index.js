833c4d5829f825152ead705c101e1388628a0b64
Vincent M. van der Meulen <meulen@uw.edu>
2018-02-26 21:38:02 -0800

    Add default components

---
+++ clients/web/src/components/Circle/index.js
@@ -0,0 +1,19 @@
+// Adaptation of https://raw.githubusercontent.com/jxnblk/rebass/master/src/Circle.js
+
+import sys from "system-components";
+import Badge from "../Badge";
+
+export const Circle = sys(
+    {
+        is: Badge,
+        size: 24,
+        align: "center",
+        borderRadius: "99999px"
+    },
+    "textAlign",
+    "size"
+);
+
+Circle.displayName = "Circle";
+
+export default Circle;
---
+++ clients/web/src/components/Divider/index.js
@@ -0,0 +1,20 @@
+// Adaptation of https://raw.githubusercontent.com/jxnblk/rebass/master/src/Divider.js
+
+import sys from "system-components";
+
+export const Divider = sys(
+    {
+        is: "hr",
+        mx: 0,
+        my: 3,
+        border: 0,
+        borderBottom: 1,
+        borderColor: "gray"
+    },
+    "space",
+    "color"
+);
+
+Divider.displayName = "Divider";
+
+export default Divider;
