6a1e6d535f3fe7ddb557ff60c848993affb31c69
Vincent M. van der Meulen <meulen@uw.edu>
2018-02-26 19:23:43 -0800

    Start actual work

--- clients/web/src/containers/Navigation/index.js
+++ clients/web/src/containers/Navigation/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Box } from "grid-styled";
 
 class Navigation extends Component {
     render() {
         return (
-            <div>
+            <Box bg="wireframe" py={3}>
                 <Link to="/">AirliftNW</Link>
                 <Link to="/missions">Missions</Link>
                 <Link to="/personnel">Personnel</Link>
-            </div>
+            </Box>
         );
     }
 }
--- clients/web/src/theme.js
+++ clients/web/src/theme.js
@@ -1,3 +1,7 @@
-const theme = {};
+const theme = {
+    colors: {
+        wireframe: "#d8d8d8"
+    }
+};
 
 export default theme;
