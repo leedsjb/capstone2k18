75b3903b2237c27dbf8ee2234664e52f050ce307
Vincent M. van der Meulen <meulen@uw.edu>
2018-03-23 14:54:46 -0700

    Remove unused vars

--- clients/web/src/components/MissionListItem/index.js
+++ clients/web/src/components/MissionListItem/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import Box from "../../components/Box";
-
 const MissionListItem = () => {
     return <Link to="/missions/test">Mission List item</Link>;
 };
