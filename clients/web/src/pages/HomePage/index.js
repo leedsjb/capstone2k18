b326e5dd2411d2a8887268209b29e93b5b3b2792
Vincent M. van der Meulen <meulen@uw.edu>
2018-04-02 11:54:09 -0700

    Add feature placeholder to home page

--- clients/web/src/pages/HomePage/index.js
+++ clients/web/src/pages/HomePage/index.js
@@ -5,6 +5,7 @@ import Box from "../../components/Box";
 import Heading from "../../components/Heading";
 import Container from "../../components/Container";
 import Logo from "../../components/Logo";
+import Measure from "../../components/Measure";
 
 const HomePage = () => {
     return (
@@ -17,7 +18,26 @@ const HomePage = () => {
             </Box>
             <Container>
                 <Heading is="h1">AirliftNW Elevate</Heading>
-                <div>Home Page</div>
+
+                <Heading is="h2" fontSize={4}>
+                    Feature One
+                </Heading>
+                <Measure>
+                    Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet
+                    rutrum faucibus dolor auctor. Integer posuere erat a ante
+                    venenatis dapibus posuere velit aliquet. Sed posuere
+                    consectetur est at lobortis.
+                </Measure>
+
+                <Heading is="h2" fontSize={4}>
+                    Feature Two
+                </Heading>
+                <Measure>
+                    Maecenas sed diam eget risus varius blandit sit amet non
+                    magna. Nulla vitae elit libero, a pharetra augue. Donec
+                    ullamcorper nulla non metus auctor fringilla. Nulla vitae
+                    elit libero, a pharetra augue.
+                </Measure>
             </Container>
         </div>
     );
