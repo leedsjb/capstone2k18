d78142a5219f5af6538dca532b435fdee4238a4c
Vincent M. van der Meulen <meulen@uw.edu>
2018-04-03 22:06:31 -0700

    Start on redux actions

--- clients/web/src/pages/MissionsPage/index.js
+++ clients/web/src/pages/MissionsPage/index.js
@@ -25,6 +25,8 @@ import Heading from "../../components/Heading";
 import Measure from "../../components/Measure";
 import MasterDetailMapView from "../../components/MasterDetailMapView";
 
+import MissionsProvider from "../../containers/MissionsProvider";
+
 const Map = ReactMapboxGl({
     accessToken: process.env.REACT_APP_MAPBOX
 });
@@ -80,100 +82,126 @@ class MissionsPage extends Component {
 
     render() {
         return (
-            <FlexFullHeight flexDirection="column">
-                <Helmet>
-                    <title>Missions</title>
-                </Helmet>
+            <MissionsProvider
+                render={({ missions }) => {
+                    return (
+                        <FlexFullHeight flexDirection="column">
+                            <Helmet>
+                                <title>Missions</title>
+                            </Helmet>
 
-                <TitleBar
-                    title="Missions"
-                    icon="map"
-                    iconOnClick={this.toggleListView}
-                />
+                            <TitleBar
+                                title="Missions"
+                                icon="map"
+                                iconOnClick={this.toggleListView}
+                            />
 
-                <NavBar />
+                            <NavBar />
 
-                <MasterDetailMapView
-                    renderMasterView={this.renderMissions}
-                    renderDetailView={() => {
-                        return (
-                            <div>
-                                <Heading is="h3">Information</Heading>
-                                <Measure>
-                                    Integer posuere erat a ante venenatis
-                                    dapibus posuere velit aliquet. Donec
-                                    ullamcorper nulla non metus auctor
-                                    fringilla. Cum sociis natoque penatibus et
-                                    magnis dis parturient montes, nascetur
-                                    ridiculus mus. Duis mollis, est non commodo
-                                    luctus, nisi erat porttitor ligula, eget
-                                    lacinia odio sem nec elit. Lorem ipsum dolor
-                                    sit amet, consectetur adipiscing elit. Morbi
-                                    leo risus, porta ac consectetur ac,
-                                    vestibulum at eros. Fusce dapibus, tellus ac
-                                    cursus commodo, tortor mauris condimentum
-                                    nibh, ut fermentum massa justo sit amet
-                                    risus.
-                                </Measure>
-                                <Heading is="h3">Information</Heading>
-                                <Measure>
-                                    Integer posuere erat a ante venenatis
-                                    dapibus posuere velit aliquet. Donec
-                                    ullamcorper nulla non metus auctor
-                                    fringilla. Cum sociis natoque penatibus et
-                                    magnis dis parturient montes, nascetur
-                                    ridiculus mus. Duis mollis, est non commodo
-                                    luctus, nisi erat porttitor ligula, eget
-                                    lacinia odio sem nec elit. Lorem ipsum dolor
-                                    sit amet, consectetur adipiscing elit. Morbi
-                                    leo risus, porta ac consectetur ac,
-                                    vestibulum at eros. Fusce dapibus, tellus ac
-                                    cursus commodo, tortor mauris condimentum
-                                    nibh, ut fermentum massa justo sit amet
-                                    risus.
-                                </Measure>
-                                <Heading is="h3">Information</Heading>
-                                <Measure>
-                                    Integer posuere erat a ante venenatis
-                                    dapibus posuere velit aliquet. Donec
-                                    ullamcorper nulla non metus auctor
-                                    fringilla. Cum sociis natoque penatibus et
-                                    magnis dis parturient montes, nascetur
-                                    ridiculus mus. Duis mollis, est non commodo
-                                    luctus, nisi erat porttitor ligula, eget
-                                    lacinia odio sem nec elit. Lorem ipsum dolor
-                                    sit amet, consectetur adipiscing elit. Morbi
-                                    leo risus, porta ac consectetur ac,
-                                    vestibulum at eros. Fusce dapibus, tellus ac
-                                    cursus commodo, tortor mauris condimentum
-                                    nibh, ut fermentum massa justo sit amet
-                                    risus.
-                                </Measure>
-                            </div>
-                        );
-                    }}
-                    renderMapView={() => {
-                        return (
-                            <Layer
-                                type="symbol"
-                                id="marker"
-                                layout={{
-                                    "icon-image": "marker-15"
+                            <MasterDetailMapView
+                                renderMasterView={() =>
+                                    this.renderMissions(missions.data)
+                                }
+                                renderDetailView={() => {
+                                    return (
+                                        <div>
+                                            <Heading is="h3">
+                                                Information
+                                            </Heading>
+                                            <Measure>
+                                                Integer posuere erat a ante
+                                                venenatis dapibus posuere velit
+                                                aliquet. Donec ullamcorper nulla
+                                                non metus auctor fringilla. Cum
+                                                sociis natoque penatibus et
+                                                magnis dis parturient montes,
+                                                nascetur ridiculus mus. Duis
+                                                mollis, est non commodo luctus,
+                                                nisi erat porttitor ligula, eget
+                                                lacinia odio sem nec elit. Lorem
+                                                ipsum dolor sit amet,
+                                                consectetur adipiscing elit.
+                                                Morbi leo risus, porta ac
+                                                consectetur ac, vestibulum at
+                                                eros. Fusce dapibus, tellus ac
+                                                cursus commodo, tortor mauris
+                                                condimentum nibh, ut fermentum
+                                                massa justo sit amet risus.
+                                            </Measure>
+                                            <Heading is="h3">
+                                                Information
+                                            </Heading>
+                                            <Measure>
+                                                Integer posuere erat a ante
+                                                venenatis dapibus posuere velit
+                                                aliquet. Donec ullamcorper nulla
+                                                non metus auctor fringilla. Cum
+                                                sociis natoque penatibus et
+                                                magnis dis parturient montes,
+                                                nascetur ridiculus mus. Duis
+                                                mollis, est non commodo luctus,
+                                                nisi erat porttitor ligula, eget
+                                                lacinia odio sem nec elit. Lorem
+                                                ipsum dolor sit amet,
+                                                consectetur adipiscing elit.
+                                                Morbi leo risus, porta ac
+                                                consectetur ac, vestibulum at
+                                                eros. Fusce dapibus, tellus ac
+                                                cursus commodo, tortor mauris
+                                                condimentum nibh, ut fermentum
+                                                massa justo sit amet risus.
+                                            </Measure>
+                                            <Heading is="h3">
+                                                Information
+                                            </Heading>
+                                            <Measure>
+                                                Integer posuere erat a ante
+                                                venenatis dapibus posuere velit
+                                                aliquet. Donec ullamcorper nulla
+                                                non metus auctor fringilla. Cum
+                                                sociis natoque penatibus et
+                                                magnis dis parturient montes,
+                                                nascetur ridiculus mus. Duis
+                                                mollis, est non commodo luctus,
+                                                nisi erat porttitor ligula, eget
+                                                lacinia odio sem nec elit. Lorem
+                                                ipsum dolor sit amet,
+                                                consectetur adipiscing elit.
+                                                Morbi leo risus, porta ac
+                                                consectetur ac, vestibulum at
+                                                eros. Fusce dapibus, tellus ac
+                                                cursus commodo, tortor mauris
+                                                condimentum nibh, ut fermentum
+                                                massa justo sit amet risus.
+                                            </Measure>
+                                        </div>
+                                    );
                                 }}
-                            >
-                                <Feature
-                                    coordinates={[
-                                        -0.481747846041145,
-                                        51.3233379650232
-                                    ]}
-                                />
-                            </Layer>
-                        );
-                    }}
-                />
+                                renderMapView={() => {
+                                    return (
+                                        <Layer
+                                            type="symbol"
+                                            id="marker"
+                                            layout={{
+                                                "icon-image": "marker-15"
+                                            }}
+                                        >
+                                            <Feature
+                                                coordinates={[
+                                                    -0.481747846041145,
+                                                    51.3233379650232
+                                                ]}
+                                            />
+                                        </Layer>
+                                    );
+                                }}
+                            />
 
-                <TabBar />
-            </FlexFullHeight>
+                            <TabBar />
+                        </FlexFullHeight>
+                    );
+                }}
+            />
         );
     }
 }
