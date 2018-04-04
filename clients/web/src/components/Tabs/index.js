e04232377e8e91e409a5308ef2585c0ddec3c3cf
Jessica Basa <jdbasa24@uw.edu>
2018-04-03 22:17:42 -0700

    Made Tabs component stateless

--- clients/web/src/components/Tabs/index.js
+++ clients/web/src/components/Tabs/index.js
@@ -2,27 +2,23 @@ import React, { Component } from "react";
 
 import Tab from "../../components/Tab";
 
-class Tabs extends Component {
-    render() {
-        return (
-            <div>
-                {React.Children.map(this.props.children, (child, i) => {
-                    return (
-                        <Tab
-                            active={
-                                child.key === this.props.active ? true : false
-                            }
-                            onClick={() => {
-                                this.props.onChange(child.key);
-                            }}
-                        >
-                            {child}
-                        </Tab>
-                    );
-                })}
-            </div>
-        );
-    }
-}
+const Tabs = props => {
+    return (
+        <div>
+            {React.Children.map(props.children, (child, i) => {
+                return (
+                    <Tab
+                        active={child.key === props.active ? true : false}
+                        onClick={() => {
+                            props.onChange(child.key);
+                        }}
+                    >
+                        {child}
+                    </Tab>
+                );
+            })}
+        </div>
+    );
+};
 
 export default Tabs;
