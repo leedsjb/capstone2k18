33e2be6fc4230a49c26017bec4d66997772bdff8
J. Benjamin Leeds <leeds@uw.edu>
2018-04-05 23:28:55 -0700

    update build-deploy-script to include yarn steps

--- clients/web/kubernetes_deployment/build-deploy-image.sh
+++ clients/web/kubernetes_deployment/build-deploy-image.sh
@@ -2,11 +2,18 @@
 # Use this script to build the docker image and deploy it to the google container registry
 # Run from kubernetes_deployment folder for Dockerfile filepath to work
 
+docker stop web_container # stop any old running container
+# docker rm web_container # remove any old container
+docker rmi us.gcr.io/airliftnw-uw/webclient # delete existing image
+
+yarn install # ensure all necessary dependencies are installed
+yarn run build # build production version of react web app
+
 docker build -t us.gcr.io/airliftnw-uw/webclient ../ #move to dir containing Dockerfile
 
 # upload conatiner to google container registry (so Kubernetes Engine can download and run it)
-gcloud docker -- push gcr.io/airliftnw-uw/webclient
+gcloud docker -- push us.gcr.io/airliftnw-uw/webclient # note command deprecated in Docker 18.03+
 
 # optional step - run container locally to test image
-# docker run --rm -p 8080:8080 gcr.io/airliftnw-uw/webclient
+# docker run --rm -d -p 8080:8080 --name web_container us.gcr.io/airliftnw-uw/webclient
 # curl http://localhost:8080
