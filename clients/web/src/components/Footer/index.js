bed97f0a1a870fad889e76b3c28ab047405f332f
Vincent M. van der Meulen <meulen@uw.edu>
2018-04-05 20:19:46 -0700

    Add home page placeholder

---
+++ clients/web/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Text from "../Text";
+import Container from "../Container";
+
+const Footer = () => {
+    return (
+        <Container py={5}>
+            <Text my={0}>Donec id elit non mi porta gravida at eget metus</Text>
+        </Container>
+    );
+};
+
+export default Footer;
--- clients/web/src/pages/HomePage/index.js
+++ clients/web/src/pages/HomePage/index.js
@@ -6,6 +6,8 @@ import Heading from "../../components/Heading";
 import Container from "../../components/Container";
 import Logo from "../../components/Logo";
 import Measure from "../../components/Measure";
+import Button from "../../components/Button";
+import Footer from "../../components/Footer";
 
 const HomePage = () => {
     return (
@@ -16,29 +18,53 @@ const HomePage = () => {
                     <span>Sign in</span>
                 </Flex>
             </Box>
-            <Container>
-                <Heading is="h1">AirliftNW Elevate</Heading>
-
-                <Heading is="h2" fontSize={4}>
-                    Feature One
+            <Container py={5}>
+                <Heading is="h1" mt={0}>
+                    AirliftNW Elevate
                 </Heading>
-                <Measure>
-                    Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet
-                    rutrum faucibus dolor auctor. Integer posuere erat a ante
-                    venenatis dapibus posuere velit aliquet. Sed posuere
+                <Measure mt={3}>
+                    Nullam quis risus eget urna mollis ornare vel eu leo. Nullam
+                    quis risus eget urna mollis ornare vel eu leo. Donec id elit
+                    non mi porta gravida at eget metus. Maecenas sed diam eget
+                    risus varius blandit sit amet non magna. Sed posuere
                     consectetur est at lobortis.
                 </Measure>
-
-                <Heading is="h2" fontSize={4}>
-                    Feature Two
-                </Heading>
-                <Measure>
-                    Maecenas sed diam eget risus varius blandit sit amet non
-                    magna. Nulla vitae elit libero, a pharetra augue. Donec
-                    ullamcorper nulla non metus auctor fringilla. Nulla vitae
-                    elit libero, a pharetra augue.
-                </Measure>
+                <Button mt={3}>Call to action</Button>
             </Container>
+            <Box bg="wireframe">
+                <Container py={5}>
+                    <Heading is="h2" fontSize={4} mt={0}>
+                        Feature One
+                    </Heading>
+                    <Measure mt={2}>
+                        Donec sed odio dui. Vivamus sagittis lacus vel augue
+                        laoreet rutrum faucibus dolor auctor. Integer posuere
+                        erat a ante venenatis dapibus posuere velit aliquet. Sed
+                        posuere consectetur est at lobortis.
+                    </Measure>
+
+                    <Heading is="h2" fontSize={4} mt={4}>
+                        Feature Two
+                    </Heading>
+                    <Measure mt={2}>
+                        Maecenas sed diam eget risus varius blandit sit amet non
+                        magna. Nulla vitae elit libero, a pharetra augue. Donec
+                        ullamcorper nulla non metus auctor fringilla. Nulla
+                        vitae elit libero, a pharetra augue.
+                    </Measure>
+
+                    <Heading is="h2" fontSize={4} mt={4}>
+                        Feature Three
+                    </Heading>
+                    <Measure mt={2} mb={0}>
+                        Maecenas sed diam eget risus varius blandit sit amet non
+                        magna. Nulla vitae elit libero, a pharetra augue. Donec
+                        ullamcorper nulla non metus auctor fringilla. Nulla
+                        vitae elit libero, a pharetra augue.
+                    </Measure>
+                </Container>
+            </Box>
+            <Footer />
         </div>
     );
 };
