0060b23f803358a98ead1354a455500d62172eea
Tiffany <tzchen@uw.edu>
2018-04-05 20:53:12 -0700

    specify Google Container Registry to be US

--- clients/web/kubernetes_deployment/build-deploy-image.sh
+++ clients/web/kubernetes_deployment/build-deploy-image.sh
@@ -5,8 +5,8 @@
 docker build -t us.gcr.io/airliftnw-uw/webclient ../ #move to dir containing Dockerfile
 
 # upload conatiner to google container registry (so Kubernetes Engine can download and run it)
-gcloud docker -- push gcr.io/airliftnw-uw/webclient
+gcloud docker -- push us.gcr.io/airliftnw-uw/webclient
 
 # optional step - run container locally to test image
-# docker run --rm -p 8080:8080 gcr.io/airliftnw-uw/webclient
+# docker run --rm -p 8080:8080 us.gcr.io/airliftnw-uw/webclient
 # curl http://localhost:8080
