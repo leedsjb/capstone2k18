a4803cdd6b0f81a61d4427facad2d376b8209b33
Jessica Basa <jdbasa24@uw.edu>
2018-04-20 14:19:10 -0700

    Added more information to AircraftListItem

--- clients/web/src/components/AircraftListItem/index.js
+++ clients/web/src/components/AircraftListItem/index.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Flex } from "grid-styled";
 
+import Badge from "../Badge";
+import Box from "../Box";
 import MasterListItem from "../MasterListItem";
+import Span from "../Span";
 
 const AircraftListItem = ({ aircraft }) => {
-    return <MasterListItem>{aircraft.callsign}</MasterListItem>;
+    return (
+        <MasterListItem>
+            <Box>
+                <Flex alignItems="center">
+                    <Box mr={2}>
+                        <Span fontWeight="bold">{aircraft.callsign}</Span>
+                    </Box>
+                    <Box mr={1}>
+                        <Span>{aircraft.nNum}</Span>
+                    </Box>
+                    <Badge>{aircraft.status}</Badge>
+                </Flex>
+            </Box>
+        </MasterListItem>
+    );
 };
 
 export default AircraftListItem;
