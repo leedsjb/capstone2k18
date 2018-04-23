992ae97f74c10a3c3f345419347e52a2f0b2a7b7
Tiffany <tzchen@uw.edu>
2018-04-22 22:14:21 -0700

    connect to Cloud SQL, but with segfaults (mongodb?)

--- servers/gateway/main.go
+++ servers/gateway/main.go
@@ -8,9 +8,13 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"os"
-	"strings"
+	// "strings"
 	"sync"
 	"time"
+	
+	// sql + go sql driver
+	// "database/sql"
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/leedsjb/capstone2k18/servers/gateway/indexes"
 
@@ -23,6 +27,8 @@ import (
 	"github.com/go-redis/redis"
 
 	"github.com/leedsjb/capstone2k18/servers/gateway/handlers"
+
+	"github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/mysql"
 )
 
 const (
@@ -106,25 +112,25 @@ func main() {
 			os.Exit(1)
 		}
 
-	// TODO: Listen to Pub/Sub where we (subscriber) are listening to published msgs 
-	//may contain a comma-delimited list of network addresses where
-	//messaging microservice instances are listening
-	messagesSvcAddrs := os.Getenv("MESSAGESSVCADDR")
-	if len(messagesSvcAddrs) == 0 {
-		fmt.Println("Please provide a list of messaging microservice addresses")
-		os.Exit(1)
-	}
+	// // TODO: Listen to Pub/Sub where we (subscriber) are listening to published msgs 
+	// //may contain a comma-delimited list of network addresses where
+	// //messaging microservice instances are listening
+	// messagesSvcAddrs := os.Getenv("MESSAGESSVCADDR")
+	// if len(messagesSvcAddrs) == 0 {
+	// 	fmt.Println("Please provide a list of messaging microservice addresses")
+	// 	os.Exit(1)
+	// }
 
-	// TODO: load balance message addresses
-	splitMessagesSvcAddrs := strings.Split(messagesSvcAddrs, ",")
-	//may contain a comma-delimited list of network addresses where
-	//page summary microservice instances are listening
-	summarySvcAddrs := os.Getenv("SUMMARYSVCADDR")
-	if len(summarySvcAddrs) == 0 {
-		fmt.Println("Please provide a list of summary microservice addresses")
-		os.Exit(1)
-	}
-	splitSummarySvcAddrs := strings.Split(summarySvcAddrs, ",")
+	// // TODO: load balance message addresses
+	// splitMessagesSvcAddrs := strings.Split(messagesSvcAddrs, ",")
+	// //may contain a comma-delimited list of network addresses where
+	// //page summary microservice instances are listening
+	// summarySvcAddrs := os.Getenv("SUMMARYSVCADDR")
+	// if len(summarySvcAddrs) == 0 {
+	// 	fmt.Println("Please provide a list of summary microservice addresses")
+	// 	os.Exit(1)
+	// }
+	// splitSummarySvcAddrs := strings.Split(summarySvcAddrs, ",")
 
 	//Use the REDISADDR to create a new redis Client, which you can pass to your
 	//sessions.NewRedisStore() function.
@@ -133,9 +139,71 @@ func main() {
 	// TODO: not MongoDB -> how to connect to MySQL
 	//Use the DBADDR to dial MongoDB server
 	sess, err := mgo.Dial(dbAddr)
+
+	sqlInstance := os.Getenv("SQLINSTANCE")
+	sqlUser := os.Getenv("SQLUSER")
+	sqlPass := os.Getenv("SQLPASS")
+	sqlDbName := os.Getenv("SQLDBNAME")
+	sqlTblName := os.Getenv("SQLTABLENAME")
+
+	cfg := mysql.Cfg(sqlInstance, sqlUser, sqlPass)
+	cfg.DBName = sqlDbName
+	db, err := mysql.DialCfg(cfg)
 	if err != nil {
-		fmt.Printf("Error dialing Mongo: %v", err)
+		fmt.Printf("Error dialing MySQL: %v", err)
 		os.Exit(1)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM " + sqlTblName)
+	if err != nil {
+		fmt.Printf("Error querying MySQL: %v", err)
+		os.Exit(1)
+	}
+
+	// TODO: DO SOMETHING WITH ROWS
+	i := 0
+	for rows.Next() {
+		i = i + 1
+		fmt.Println(i)
+		// var aircraft_lat int
+		// var aircraft_long string
+		// var model_title string
+		// var model_desc string
+		// var aircraft_category string
+		// var aircraft_callsign string
+		// var mission_date string
+		// var agency_name string
+		// var agency_area_code string
+		// var agency_phone string
+		// var adderss_street string
+		// var address_city string
+		// var address_state string
+		// var address_zip string
+		// err := rows.Scan(
+		// 	&aircraft_lat,
+		// 	&aircraft_long,
+		// 	&model_title,
+		// 	&model_desc,
+		// 	&aircraft_category,
+		// 	&aircraft_callsign,
+		// 	&mission_date,
+		// 	&agency_name,
+		// 	&agency_area_code,
+		// 	&address_state,
+		// 	&address_zip,
+		// 	&address_city,
+		// 	&address_state,
+		// 	&address_zip
+		// )
+		// if err != nil {
+		// 	fmt.Printf("Error parsing MySQL rows: %v", err)
+		// 	os.Exit(1)
+		// }
+		// fmt.Println(uid)
+		// fmt.Println(username)
+		// fmt.Println(department)
+		// fmt.Println(created)
 	}
 
 	//Initialize user store.
@@ -147,6 +215,7 @@ func main() {
 	//Create and initialize a new instance of your handler context struct with
 	//the signing key, the sesson store, and the user store.
 	handlerCtx := handlers.NewHandlerContext(sessionKey, sessionStore, userStore, trie)
+	// handlerCtx := handlers.NewHandlerContext(sessionKey, sessionStore, trie)
 	if err := handlerCtx.UserStore.LoadTrie(trie); err != nil {
 		fmt.Printf("Error loading trie: %v", err)
 		os.Exit(1)
@@ -155,15 +224,15 @@ func main() {
 	//Create a new mux
 	mux := http.NewServeMux()
 
-	//Add microservice handlers
-	///v1/summary
-	mux.Handle(apiRoot+"summary", NewServiceProxy(splitSummarySvcAddrs, sessionKey, sessionStore))
-	///v1/channels
-	mux.Handle(apiRoot+"channels", NewServiceProxy(splitMessagesSvcAddrs, sessionKey, sessionStore))
-	mux.Handle(apiRoot+"channels/", NewServiceProxy(splitMessagesSvcAddrs, sessionKey, sessionStore))
-	///v1/messages
-	mux.Handle(apiRoot+"messages", NewServiceProxy(splitMessagesSvcAddrs, sessionKey, sessionStore))
-	mux.Handle(apiRoot+"messages/", NewServiceProxy(splitMessagesSvcAddrs, sessionKey, sessionStore))
+	// //Add microservice handlers
+	// // /v1/summary
+	// mux.Handle(apiRoot+"summary", NewServiceProxy(splitSummarySvcAddrs, sessionKey, sessionStore))
+	// // /v1/channels
+	// mux.Handle(apiRoot+"channels", NewServiceProxy(splitMessagesSvcAddrs, sessionKey, sessionStore))
+	// mux.Handle(apiRoot+"channels/", NewServiceProxy(splitMessagesSvcAddrs, sessionKey, sessionStore))
+	// ///v1/messages
+	// mux.Handle(apiRoot+"messages", NewServiceProxy(splitMessagesSvcAddrs, sessionKey, sessionStore))
+	// mux.Handle(apiRoot+"messages/", NewServiceProxy(splitMessagesSvcAddrs, sessionKey, sessionStore))
 
 	//Add regular handlers
 	///v1/users: UsersHandler
