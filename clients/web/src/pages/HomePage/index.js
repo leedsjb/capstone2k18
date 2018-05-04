75ef449652b5a0d2f9512998e26abea901c581c5
Vincent M. van der Meulen <meulen@uw.edu>
2018-05-04 08:20:51 -0700

    Work on landing page

--- clients/web/src/pages/HomePage/index.js
+++ clients/web/src/pages/HomePage/index.js
@@ -12,58 +12,82 @@ import Heading from "../../components/Heading";
 import Image from "../../components/Image";
 import Logo from "../../components/Logo";
 import Measure from "../../components/Measure";
+import RedBox from "../../components/RedBox";
 import Span from "../../components/Span";
 import Text from "../../components/Text";
 
 import placeholder from "../../images/placeholder-image.jpg";
+import phone from "../../images/phone.png";
+import logo from "../../images/logo.svg";
+import second from "../../images/second.png";
 
 const HomePage = () => {
     return (
         <div>
-            <Box p={3} px={5}>
-                <Flex justifyContent="space-between">
-                    <Link to="/">
-                        <Logo />
-                    </Link>
-                    <Button>
-                        <Link to="/signin">Sign in</Link>
-                    </Button>
-                </Flex>
+            <Box bg="#F7F9FC">
+                <Container>
+                    <Flex
+                        justifyContent="space-between"
+                        alignItems="center"
+                        pt={3}
+                    >
+                        <Link to="/">
+                            <Image src={logo} alt="AirliftNW Elevate" w={183} />
+                        </Link>
+                        <div>
+                            <Button>
+                                <Link to="/signin">Sign in</Link>
+                            </Button>
+                        </div>
+                    </Flex>
+
+                    <Flex
+                        flexWrap={["wrap", "wrap", "nowrap"]}
+                        justifyContent="space-between"
+                        alignItems="center"
+                    >
+                        <Box width={[1, 1, 2 / 3]}>
+                            <Heading is="h1" mt={0} fontSize={6}>
+                                Critical Communication for Airborne Medical
+                                Missions
+                            </Heading>
+                            <Measure mt={3} mb={0}>
+                                Elevate is a mission-critical, high-availability
+                                application for Airlift Northwest emergency
+                                flight nurses and pilots transporting severely
+                                ill or injured patients by helicopter to reach
+                                life-saving medical treatment.
+                            </Measure>
+                            <Button mt={3}>Get Started</Button>
+                        </Box>
+                        <Box my={5} width={[1, 1, 1 / 3]}>
+                            <Image src={phone} width={1} />
+                        </Box>
+                    </Flex>
+                </Container>
             </Box>
 
-            <Flex flexWrap={["wrap", "wrap", "nowrap"]}>
-                <Box mr={[0, 0, 5]} width={[1, 1, 2 / 3]}>
-                    <Container px={5} py={5}>
-                        <Heading is="h1" mt={0}>
-                            Critical Communication for Airborne Medical Missions
-                        </Heading>
-                        <Measure mt={3} mb={0}>
-                            Elevate is a mission-critical, high-availability
-                            application for Airlift Northwest emergency flight
-                            nurses and pilots transporting severely ill or
-                            injured patients by helicopter to reach life-saving
-                            medical treatment.
-                        </Measure>
-                        <Button mt={3}>Get Started</Button>
-                    </Container>
-                </Box>
-                <Box mr={[0, 0, 5]} my={5} width={[1, 1, 1 / 3]}>
-                    <Image src={placeholder} width={1} />
-                </Box>
-            </Flex>
-
-            <Box bg="wireframe">
-                <Container px={5} py={5}>
-                    <Box>
-                        <Flex flexWrap={["wrap", "wrap", "nowrap"]}>
-                            <Box mr={[0, 0, 5]}>
-                                <Image src={placeholder} width={1} />
+            <RedBox>
+                <Container>
+                    <Box py={6}>
+                        <Flex
+                            flexWrap={["wrap", "wrap", "nowrap"]}
+                            justifyContent="space-between"
+                            alignItems="center"
+                        >
+                            <Box maxWidth={488}>
+                                <Image src={second} width={1} />
                             </Box>
                             <Box>
-                                <Heading is="h2" fontSize={4} pt={4}>
+                                <Heading
+                                    is="h2"
+                                    fontSize={4}
+                                    pt={4}
+                                    color="white"
+                                >
                                     Because every second counts
                                 </Heading>
-                                <Measure mt={2}>
+                                <Measure mt={2} color="white">
                                     Elevate bridges the communication gap
                                     between the dispatch center on the ground
                                     and flight crews in the air by displaying
@@ -77,11 +101,11 @@ const HomePage = () => {
                         </Flex>
                     </Box>
                 </Container>
-            </Box>
+            </RedBox>
 
             <Box>
                 <Container px={5} py={5}>
-                    <Heading is="h2" fontSize={4} mb={4} mt={2}>
+                    <Heading is="h2" fontSize={5} mb={4} mt={2}>
                         Elevating health care technology
                     </Heading>
                     <Flex flexWrap={["wrap", "wrap", "nowrap"]}>
@@ -124,7 +148,7 @@ const HomePage = () => {
                 </Container>
             </Box>
 
-            <Box bg="wireframe">
+            <RedBox>
                 <Container px={5} py={5}>
                     <Box>
                         <Flex
@@ -134,12 +158,18 @@ const HomePage = () => {
                                 "column-reverse",
                                 "row"
                             ]}
+                            alignItems="center"
                         >
                             <Box mr={[0, 0, 5]}>
-                                <Heading is="h2" fontSize={4} pt={5}>
+                                <Heading
+                                    is="h2"
+                                    fontSize={4}
+                                    pt={5}
+                                    color="white"
+                                >
                                     Track missions
                                 </Heading>
-                                <Measure mt={2}>
+                                <Measure mt={2} color="white">
                                     Duis mollis, est non commodo luctus, nisis
                                     erat porttitor ligula, eget lacinia odio sem
                                     nec elit. Nulla vitae elit libero, a
@@ -147,40 +177,41 @@ const HomePage = () => {
                                     fellis euismod semper.
                                 </Measure>
                             </Box>
-                            <Box width={1}>
+                            <Box maxWidth={488}>
                                 <Image src={placeholder} width={1} />
                             </Box>
                         </Flex>
                     </Box>
                 </Container>
-            </Box>
+            </RedBox>
 
-            <Box>
-                <Container px={5} py={5}>
-                    <Box>
-                        <Flex flexWrap={["wrap", "wrap", "nowrap"]}>
-                            <Box mr={[0, 0, 5]}>
-                                <Image src={placeholder} width={1} />
-                            </Box>
-                            <Box>
-                                <Heading is="h2" fontSize={4} pt={5}>
-                                    Get notified
-                                </Heading>
-                                <Measure mt={2}>
-                                    Duis mollis, est non commodo luctus, nisis
-                                    erat porttitor ligula, eget lacinia odio sem
-                                    nec elit. Nulla vitae elit libero, a
-                                    pharetra augue. Vistibulum id lignula port
-                                    fellis euismod semper.
-                                </Measure>
-                            </Box>
-                        </Flex>
-                    </Box>
+            <Box bg="#F7F9FC">
+                <Container>
+                    <Flex
+                        flexWrap={["wrap", "wrap", "nowrap"]}
+                        alignItems="center"
+                    >
+                        <Box maxWidth={488}>
+                            <Image src={placeholder} width={1} />
+                        </Box>
+                        <Box>
+                            <Heading is="h2" fontSize={4} pt={5}>
+                                Get notified
+                            </Heading>
+                            <Measure mt={2}>
+                                Duis mollis, est non commodo luctus, nisis erat
+                                porttitor ligula, eget lacinia odio sem nec
+                                elit. Nulla vitae elit libero, a pharetra augue.
+                                Vistibulum id lignula port fellis euismod
+                                semper.
+                            </Measure>
+                        </Box>
+                    </Flex>
                 </Container>
             </Box>
 
-            <Box bg="wireframe">
-                <Container px={5} py={5}>
+            <RedBox>
+                <Container py={5}>
                     <Box>
                         <Flex
                             flexWrap={["wrap", "wrap", "nowrap"]}
@@ -189,13 +220,19 @@ const HomePage = () => {
                                 "column-reverse",
                                 "row"
                             ]}
+                            alignItems="center"
                         >
                             <Box mr={[0, 0, 5]}>
-                                <Heading is="h2" fontSize={4} pt={5}>
+                                <Heading
+                                    is="h2"
+                                    fontSize={4}
+                                    pt={5}
+                                    color="white"
+                                >
                                     Find the right person
                                 </Heading>
-                                <Measure mt={2}>
-                                    Duis mollis, est non commodo luctus, nisis
+                                <Measure mt={2} color="white">
+                                    > Duis mollis, est non commodo luctus, nisis
                                     erat porttitor ligula, eget lacinia odio sem
                                     nec elit. Nulla vitae elit libero, a
                                     pharetra augue. Vistibulum id lignula port
@@ -208,12 +245,15 @@ const HomePage = () => {
                         </Flex>
                     </Box>
                 </Container>
-            </Box>
+            </RedBox>
 
             <Box>
-                <Container px={5} py={5}>
+                <Container py={5}>
                     <Box>
-                        <Flex flexWrap={["wrap", "wrap", "nowrap"]}>
+                        <Flex
+                            flexWrap={["wrap", "wrap", "nowrap"]}
+                            alignItems="center"
+                        >
                             <Box mr={[0, 0, 5]}>
                                 <Image src={placeholder} width={1} />
                             </Box>
@@ -235,7 +275,7 @@ const HomePage = () => {
                 </Container>
             </Box>
 
-            <Box bg="wireframe">
+            <RedBox>
                 <Container px={5} py={5}>
                     <Box>
                         <Heading is="h2" fontSize={4} pt={2} mb={3}>
@@ -301,7 +341,7 @@ const HomePage = () => {
                         </Flex>
                     </Box>
                 </Container>
-            </Box>
+            </RedBox>
 
             <Box mt={5}>
                 <Container width={1 / 2}>
