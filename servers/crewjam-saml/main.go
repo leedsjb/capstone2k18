20705548a97157646eb932975658cc16e58391bf
J. Benjamin Leeds <leeds@uw.edu>
2018-05-13 23:53:09 -0700

    update crewjam go web server to deploy SAML SSO on GCloud

--- servers/crewjam-saml/main.go
+++ servers/crewjam-saml/main.go
@@ -7,31 +7,99 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
 	"os"
 
 	"github.com/crewjam/saml/samlsp"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s", samlsp.Token(r.Context()).Attributes.Get("cn"))
-}
-
 /*
-
 	Generate certs:
 	openssl req -x509 -newkey rsa:2048 -keyout myservice.key -out myservice.crt -days 365 -nodes -subj "/CN=myservice.example.com"
-
 */
 
+// trivial protected page resource
+// must be signed in via SAML SSO to access
+// prints information about signed is session to page
+// from the request object
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s", samlsp.Token(r.Context()).Attributes.Get("cn"))
+
+	fmt.Println("request URI: " + r.RequestURI)
+	fmt.Println("request Method:" + r.Method)
+	fmt.Println("request URL:" + r.URL.String())
+	fmt.Println(r)
+	fmt.Println("************")
+
+	requestDump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(requestDump))
+
+	w.Write([]byte("request URI: " + r.RequestURI + "\n"))
+	w.Write([]byte("request Method:" + r.Method + "\n"))
+	w.Write([]byte("request URL:" + r.URL.String() + "\n"))
+	w.Write([]byte("request raw: " + string(requestDump) + "\n"))
+	w.Write([]byte("Google Cloud HTTPS L7 Load Balancer Health Check"))
+}
+
 func RootPathHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("request URI: " + r.RequestURI)
+	fmt.Println("request Method:" + r.Method)
+	fmt.Println("request URL:" + r.URL.String())
+	fmt.Println(r)
+	fmt.Println("************")
+
+	requestDump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(requestDump))
+
+	w.Write([]byte("request URI: " + r.RequestURI + "\n"))
+	w.Write([]byte("request Method:" + r.Method + "\n"))
+	w.Write([]byte("request URL:" + r.URL.String() + "\n"))
+	w.Write([]byte("request raw: " + string(requestDump) + "\n"))
+	w.Write([]byte("Google Cloud HTTPS L7 Load Balancer Health Check"))
 
 	if r.Method != "GET" {
 		http.Error(w, "must be a get request", http.StatusBadRequest)
 		return
 	}
+}
 
+func testPathHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("testPathHandler")
+	fmt.Println("request URI: " + r.RequestURI)
+	fmt.Println("request Method:" + r.Method)
+	fmt.Println("request URL:" + r.URL.String())
+	fmt.Println(r)
+	fmt.Println("************")
+
+	requestDump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(requestDump))
+
+	w.Write([]byte("testPathHandler\n"))
+	w.Write([]byte("request URI: " + r.RequestURI + "\n"))
+	w.Write([]byte("request Method:" + r.Method + "\n"))
+	w.Write([]byte("request URL:" + r.URL.String() + "\n"))
+	w.Write([]byte("request raw: " + string(requestDump) + "\n"))
 	w.Write([]byte("Google Cloud HTTPS L7 Load Balancer Health Check"))
+
+	if r.Method != "GET" {
+		http.Error(w, "must be a get request", http.StatusBadRequest)
+		return
+	}
 }
 
 func main() {
@@ -112,12 +180,14 @@ func main() {
 	rootURL := &url.URL{} // ****** correct usage of &?
 
 	if ENV == "kubernetes" {
-		rootURL, err = url.Parse("https://test.elevate.airliftnw.org")
+		// rootURL, err = url.Parse("https://test.elevate.airliftnw.org")
+		rootURL, err = url.Parse("http://35.201.91.31/crewjam")
 		if err != nil {
 			panic(err)
 		}
 	} else {
-		rootURL, err = url.Parse("https://crewjam-saml.test.elevate.emeloid.co")
+		// rootURL, err = url.Parse("https://crewjam-saml.test.elevate.emeloid.co")
+		rootURL, err = url.Parse("https://localhost:4430/crewjam")
 		if err != nil {
 			panic(err)
 		}
@@ -130,10 +200,17 @@ func main() {
 		IDPMetadataURL: idpMetadataURL,
 	})
 
-	app := http.HandlerFunc(hello) // define function to call for requests to http endpoint
-	http.HandleFunc("/", RootPathHandler)
-	http.Handle("/hello", samlSP.RequireAccount(app)) // direct requests to /hello to app
-	http.Handle("/saml/", samlSP)                     // direct requests to the /saml/ route to samlSP
+	mux := http.NewServeMux()
+
+	rootHandler := http.HandlerFunc(RootPathHandler)
+	mux.Handle("/", rootHandler)
+
+	mux.HandleFunc("/crewjam/testing", testPathHandler)
+
+	app := http.HandlerFunc(hello)                            // define handler to call for requests to protected path
+	mux.Handle("/crewjam/hello/", samlSP.RequireAccount(app)) // direct requests to /hello/* to hello to app and ensures clients are authenticated with SSO
+
+	mux.Handle("/crewjam/saml/", samlSP) // direct requests to the /saml/ route to samlSP
 
 	addr := host + ":" + port
 	fmt.Println("current set addr is : " + addr)
@@ -141,12 +218,13 @@ func main() {
 	var listenServeErr error
 
 	if ENV == "kubernetes" {
-		listenServeErr = http.ListenAndServe(addr, nil) // 2nd arg: handler, nil -> default serve mux
+		fmt.Println("crewjam listening at: " + addr)
+		listenServeErr = http.ListenAndServe(addr, mux) // 2nd arg: handler, nil -> default serve mux
 
-		fmt.Println("crewjam listening on port 80")
 	} else {
-		listenServeErr = http.ListenAndServeTLS(addr, tlscert, tlskey, nil)
-		fmt.Println("crewjam listening on port 443")
+		fmt.Println("crewjam listening at: " + addr)
+		listenServeErr = http.ListenAndServeTLS(addr, tlscert, tlskey, mux)
+		// listenServeErr = http.ListenAndServe(addr, nil)
 	}
 
 	if listenServeErr != nil {
