87081a534b2c23ba780604504564ae74f8def004
Tiffany <tzchen@uw.edu>
2018-05-17 18:27:44 -0700

    write skeleton functions for group parser queries

--- servers/elevate/parsers/queries.go
+++ servers/elevate/parsers/queries.go
@@ -2,6 +2,8 @@ package parsers
 
 import "github.com/leedsjb/capstone2k18/servers/elevate/models/messages"
 
+// [AIRCRAFT]
+
 func (ctx *ParserContext) AddNewAircraft(aircraftInfo *messages.Aircraft_Create) error {
 	return nil
 }
@@ -19,5 +21,19 @@ func (ctx *ParserContext) UpdateAircraftServiceSchedule(aircraftInfo *messages.A
 }
 
 func (ctx *ParserContext) UpdateAircraftPosition(aircraftInfo *messages.Aircraft_Pos_Update) error {
+	return nil
+}
+
+// [GROUPS]
+
+func (ctx *ParserContext) AddNewGroup(groupInfo *messages.Group) error {
+	return nil
+}
+
+func (ctx *ParserContext) UpdateGroup(groupInfo *messages.Group) error {
+	return nil
+}
+
+func (ctx *ParserContext) DeleteGroup(groupInfo *messages.Group) error {
 	return nil
 }
