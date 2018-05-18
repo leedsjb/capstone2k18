20293a697eb36485af9bc9b774e3372879f25ca4
Tiffany <tzchen@uw.edu>
2018-05-18 16:02:53 -0700

    update deploy version

--- servers/elevate/image-build-deploy.sh
+++ servers/elevate/image-build-deploy.sh
@@ -2,7 +2,7 @@
 
 GOOS=linux go build
 
-docker build -t us.gcr.io/airliftnw-uw/go-apiserver:0.1 .
+docker build -t us.gcr.io/airliftnw-uw/go-apiserver:0.2 .
 
 docker push us.gcr.io/airliftnw-uw/go-apiserver
 
