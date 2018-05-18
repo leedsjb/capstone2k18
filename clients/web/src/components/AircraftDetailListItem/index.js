50412ef672fc87c13fa3e1dde832ba852d8838bd
Vincent M. van der Meulen <meulen@uw.edu>
2018-05-17 20:53:06 -0700

    Fix close button

--- clients/web/src/components/AircraftDetailListItem/index.js
+++ clients/web/src/components/AircraftDetailListItem/index.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { Flex } from "grid-styled";
+import { push } from "react-router-redux";
+import { connect } from "react-redux";
 
 import Box from "../Box";
 import CrewDetailListItem from "../CrewDetailListItem";
@@ -10,66 +12,100 @@ import Badge from "../Badge";
 import AicraftIdentifier from "../AircraftIdentifier";
 import AircraftIdentifier from "../AircraftIdentifier";
 import Icon from "../Icon";
+import Clickable from "../Clickable";
 
-const AircraftDetailListItem = ({ aircraftDetail }) => {
-    let { callsign, nNum, status } = aircraftDetail.data;
-
-    return (
-        <Box>
-            <Flex py={3} justifyContent="center">
-                <AircraftIdentifier
-                    callsign={callsign}
-                    nNum={nNum}
-                    status={status}
-                />
-            </Flex>
-            <Divider />
-            <Box px={6}>
-                <Heading is="h2" fontSize={4}>
-                    ETA
-                </Heading>
-                <Heading is="h2" fontSize={4}>
-                    Patient
-                </Heading>
-                {aircraftDetail.data.crew ? (
-                    <Box>
-                        <Heading is="h2" fontSize={4}>
-                            Assigned Crew
-                        </Heading>
-                        {aircraftDetail.data.crew.people.map(c => {
-                            return (
-                                <CrewDetailListItem crewDetail={c} key={c.id} />
-                            );
-                        })}
-                    </Box>
-                ) : null}
-                {aircraftDetail.data.mission ? (
-                    <Box>
-                        <Heading is="h6" fontSize={3} my={3}>
-                            Radio Report
-                        </Heading>
-                        <Text>{aircraftDetail.data.mission.radioReport}</Text>
-                    </Box>
-                ) : null}
-                {aircraftDetail.data.mission ? (
-                    <Box>
-                        <Heading is="h6" fontSize={3} my={3}>
-                            Requestor
-                        </Heading>
-                        <Text>{aircraftDetail.data.mission.requestor}</Text>
-                    </Box>
-                ) : null}
-                {aircraftDetail.data.mission ? (
-                    <Box>
-                        <Heading is="h6" fontSize={3} my={3}>
-                            Receiver
-                        </Heading>
-                        <Text>{aircraftDetail.data.mission.receiver}</Text>
-                    </Box>
-                ) : null}
+class AircraftDetailListItem extends Component {
+    render() {
+        return (
+            <Box>
+                <Flex
+                    py={3}
+                    justifyContent="space-between"
+                    alignItems="center"
+                    px={3}
+                >
+                    <Box w={16} />
+                    <AircraftIdentifier
+                        callsign={this.props.aircraftDetail.data.callsign}
+                        nNum={this.props.aircraftDetail.data.nNum}
+                        status={this.props.aircraftDetail.data.status}
+                    />
+                    <Clickable onClick={() => this.props.push("/aircraft")}>
+                        <Icon glyph="close" size={12} />
+                    </Clickable>
+                </Flex>
+                <Divider />
+                <Box px={6}>
+                    <Heading is="h2" fontSize={4}>
+                        ETA
+                    </Heading>
+                    <Heading is="h2" fontSize={4}>
+                        Patient
+                    </Heading>
+                    {this.props.aircraftDetail.data.crew ? (
+                        <Box>
+                            <Heading is="h2" fontSize={4}>
+                                Assigned Crew
+                            </Heading>
+                            {this.props.aircraftDetail.data.crew.people.map(
+                                c => {
+                                    return (
+                                        <CrewDetailListItem
+                                            crewDetail={c}
+                                            key={c.id}
+                                        />
+                                    );
+                                }
+                            )}
+                        </Box>
+                    ) : null}
+                    {this.props.aircraftDetail.data.mission ? (
+                        <Box>
+                            <Heading is="h6" fontSize={3} my={3}>
+                                Radio Report
+                            </Heading>
+                            <Text>
+                                {
+                                    this.props.aircraftDetail.data.mission
+                                        .radioReport
+                                }
+                            </Text>
+                        </Box>
+                    ) : null}
+                    {this.props.aircraftDetail.data.mission ? (
+                        <Box>
+                            <Heading is="h6" fontSize={3} my={3}>
+                                Requestor
+                            </Heading>
+                            <Text>
+                                {
+                                    this.props.aircraftDetail.data.mission
+                                        .requestor
+                                }
+                            </Text>
+                        </Box>
+                    ) : null}
+                    {this.props.aircraftDetail.data.mission ? (
+                        <Box>
+                            <Heading is="h6" fontSize={3} my={3}>
+                                Receiver
+                            </Heading>
+                            <Text>
+                                {
+                                    this.props.aircraftDetail.data.mission
+                                        .receiver
+                                }
+                            </Text>
+                        </Box>
+                    ) : null}
+                </Box>
             </Box>
-        </Box>
-    );
+        );
+    }
+}
+
+const mapDispatchToProps = {
+    push
 };
 
-export default AircraftDetailListItem;
+export default connect(null, mapDispatchToProps)(AircraftDetailListItem);
