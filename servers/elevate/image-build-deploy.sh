d71027bea72ace49d76ae88e18b055a9570b022b
Tiffany <tzchen@uw.edu>
2018-05-19 13:42:09 -0700

    add health check handler for load balancer

--- servers/elevate/image-build-deploy.sh
+++ servers/elevate/image-build-deploy.sh
@@ -2,11 +2,11 @@
 
 GOOS=linux go build
 
-docker build -t us.gcr.io/airliftnw-uw/go-apiserver:0.6 .
+docker build -t us.gcr.io/airliftnw-uw/go-apiserver:0.9 .
 
 go clean
 
-docker push us.gcr.io/airliftnw-uw/go-apiserver:0.6
+docker push us.gcr.io/airliftnw-uw/go-apiserver:0.9
 
 ## Warning: ensure YAML spec updated to current version number
 
