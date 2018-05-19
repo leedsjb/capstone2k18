3e2171863fd2fe980ee00252b34435f8e4938686
J. Benjamin Leeds <leeds@uw.edu>
2018-05-19 15:11:19 -0700

    populate aircraft data and write aircraft stored procedures

--- servers/mysql/data-load/mysql_load_static_data.sql
+++ servers/mysql/data-load/mysql_load_static_data.sql
@@ -1,7 +1,7 @@
 /*
     mysql_load_csv_data.sql
     Created: Saturday May 12, 2018
-    Modified: Friday Mau 18, 2018
+    Modified: Saturday May 19, 2018
     Authors: J. Benjamin Leeds
     License: None
 
@@ -49,15 +49,45 @@ VALUES
 (3,2,12),
 (4,2,18)
 
-SELECT tblGROUP.group_id, group_name, personnel_f_name, personnel_l_name FROM tblGROUP
-JOIN tblPERSONNEL_GROUP ON tblGROUP.group_id = tblPERSONNEL_GROUP.group_id
-JOIN tblPERSONNEL ON tblPERSONNEL_GROUP.personnel_id = tblPERSONNEL.personnel_id
-ORDER BY tblGROUP.group_name ASC
 
-DESCRIBE tblGROUP;
+INSERT INTO tblAIRCRAFT_TYPE(
+    aircraft_type_id, aircraft_type_title, aircraft_type_desc,
+    aircraft_type_category, aircraft_type_manufacturer
+)
+INSERT INTO tblAIRCRAFT_TYPE(aircraft_type_id)
+VALUES (5)
+DESCRIBE tblAIRCRAFT_TYPE
+VALUES
+(1, "31-A", "Fastest aircraft in-fleet", "Fixed-wing", "Learjet"),
+(2, "PC-12", "More economic aircraft", "Fixed-wing", "Pilatus"),
+(3, "H-135", "Small helicopter", "Rotorcraft", "Airbus"),
+(4, "A109E", "Large helicopter", "Rotorcraft", "Augusta")
 
+INSERT INTO tblAIRCRAFT(
+    ac_id, ac_callsign, ac_n_number, ac_type_id, ac_lat, ac_long, ac_loc_display_name,
+    ac_cell_phone, ac_sat_phone
+)
+VALUES
+(1, "AL2", "N123AL", "1", "47.4441", "-122.3249", "Bremerton Base", "5555555555","4444444444"),
+(2, "AL3", "N124AL", "1", "45.5653", "-122.6448", "Seattle Children's", "5555555555","4444444444"),
+(3, "AL5", "N125AL", "2", "47.1042", "-122.87", "Harborview Medical Center", "5555555555","4444444444"),
+(4, "AL6", "N126AL", "2", "47.6849", "-122.2968", "UW Medical Center", "5555555555","4444444444"),
+(5, "AL7", "N127AL", "3", "45.5653", "-122.6448", "Yakima Base", "5555555555","4444444444"),
+(6, "AL8", "N128AL", "3", "45.5287", "-122.6363", "Boeing Field Base", "5555555555","4444444444"),
+(7, "AL9", "N129AL", "4", "47.4441", "-122.3249", "Bremerton Base", "5555555555","4444444444")
 
-DESCRIBE tblPERSONNEL_GROUP;
+INSERT INTO tblAIRCRAFT_STATUS(status_id, status_title, status_long_desc, status_short_desc)
+VALUES
+(1, "Ready", "Aircraft ready for missions", "RFM"),
+(2, "Out Of Service", "Aircraft out of service", "OOS"),
+(3, "On a Mission", "Aircraft on a mission", "OAM")
 
-DESCRIBE tblPERSONNEL;
-SELECT * FROM tblPERSONNEL;
+INSERT INTO tblASSIGNED_STATUS(aircraftstatus_id,status_id, aircraft_id)
+VALUES
+(1,1,1),
+(2,2,2),
+(3,3,3),
+(4,3,4),
+(5,3,5),
+(6,1,6),
+(7,2,7)
--- servers/mysql/stored-procedures/client_stored_procedures.sql
+++ servers/mysql/stored-procedures/client_stored_procedures.sql
@@ -1,7 +1,7 @@
 /*
 client_stored_procedures.sql
-Created: thursday May 17, 2018
-Modified:
+Created: Thursday May 17, 2018
+Modified: Saturday May 19, 2018
 Author(s): J. Benjamin Leeds
 License: None
 
@@ -9,6 +9,8 @@ Use the stored procedures in this file to retrieve data in MySQL to send to clie
 
 */
 
+-- return list of all groups
+-- endpoint: /v1/groups
 DROP PROCEDURE IF EXISTS `uspGetAllGroups`;
 CREATE PROCEDURE uspGetAllGroups()
 BEGIN
@@ -19,6 +21,8 @@ BEGIN
     ORDER BY tblGROUP.group_name ASC;
 END;
 
+-- used to populate group search trie
+-- endpoint: none
 DROP PROCEDURE IF EXISTS `uspGetGroupByID`;
 CREATE PROCEDURE uspGetGroupByID(
     IN gid INTEGER
@@ -29,4 +33,45 @@ BEGIN
     JOIN tblPERSONNEL_GROUP ON tblGROUP.group_id = tblPERSONNEL_GROUP.group_id
     JOIN tblPERSONNEL ON tblPERSONNEL_GROUP.personnel_id = tblPERSONNEL.personnel_id 
     WHERE tblGROUP.group_id = gid;
+END;
+
+-- endpoint: /v1/groups/{id}
+DROP PROCEDURE IF EXISTS `uspGetGroupDetailByID`;
+CREATE PROCEDURE uspGetGroupDetailByID(
+    IN gid INTEGER
+)
+BEGIN
+    SELECT tblGROUP.group_id, group_name, personnel_f_name,
+    personnel_l_name, tblPERSONNEL.personnel_id, personnel_title
+    FROM tblGROUP
+    JOIN tblPERSONNEL_GROUP ON tblGROUP.group_id = tblPERSONNEL_GROUP.group_id
+    JOIN tblPERSONNEL ON tblPERSONNEL_GROUP.personnel_id = tblPERSONNEL.personnel_id 
+    WHERE tblGROUP.group_id = gid;
+END;
+
+-- endpoint: /v1/aircraft
+DROP PROCEDURE IF EXISTS `uspGetAllAircraft`;
+CREATE PROCEDURE uspGetAllAircraft()
+BEGIN
+    SELECT ac_id, ac_callsign, ac_n_number, aircraft_type_manufacturer, aircraft_type_title,
+    aircraft_type_category, ac_lat, ac_long, ac_loc_display_name, status_title
+    FROM tblAIRCRAFT
+    JOIN tblAIRCRAFT_TYPE ON tblAIRCRAFT.ac_type_id = tblAIRCRAFT_TYPE.aircraft_type_id
+    JOIN tblASSIGNED_STATUS ON tblAIRCRAFT.ac_id = tblASSIGNED_STATUS.aircraft_id
+    JOIN tblAIRCRAFT_STATUS ON tblASSIGNED_STATUS.status_id = tblAIRCRAFT_STATUS.status_id;
+END;
+
+-- endpoint: /v1/aircraft/{id}
+DROP PROCEDURE IF EXISTS `uspGetAircraftByID`;
+CREATE PROCEDURE uspGetAircraftByID(
+    IN aid INTEGER
+)
+BEGIN
+ SELECT ac_id, ac_callsign, ac_n_number, aircraft_type_manufacturer, aircraft_type_title,
+    aircraft_type_category, ac_lat, ac_long, ac_loc_display_name, status_title
+    FROM tblAIRCRAFT
+    JOIN tblAIRCRAFT_TYPE ON tblAIRCRAFT.ac_type_id = tblAIRCRAFT_TYPE.aircraft_type_id
+    JOIN tblASSIGNED_STATUS ON tblAIRCRAFT.ac_id = tblASSIGNED_STATUS.aircraft_id
+    JOIN tblAIRCRAFT_STATUS ON tblASSIGNED_STATUS.status_id = tblAIRCRAFT_STATUS.status_id
+    WHERE tblAIRCRAFT.ac_id = aid;
 END;
