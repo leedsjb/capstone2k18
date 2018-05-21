0832ae050fc19431b486618065dffc53e90f3d76
Vincent M. van der Meulen <meulen@uw.edu>
2018-05-21 09:12:13 -0700

    Improve Dropdowns

---
+++ clients/web/src/components/DropdownSelect/DropdownWrapper.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+import Box from "../Box";
+
+const DropdownWrapper = styled(Box)`
+    overflow: hidden;
+`;
+
+export default DropdownWrapper;
