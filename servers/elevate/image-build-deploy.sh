d5779b1cbed9788be3036c8f9d0758297eaef939
Tiffany <tzchen@uw.edu>
2018-05-22 16:54:50 -0700

    update deployment files for v0.15, updated people previews

--- servers/elevate/image-build-deploy.sh
+++ servers/elevate/image-build-deploy.sh
@@ -2,11 +2,11 @@
 
 GOOS=linux go build
 
-docker build -t us.gcr.io/airliftnw-uw/go-apiserver:0.14 .
+docker build -t us.gcr.io/airliftnw-uw/go-apiserver:0.15 .
 
 go clean
 
-docker push us.gcr.io/airliftnw-uw/go-apiserver:0.14
+docker push us.gcr.io/airliftnw-uw/go-apiserver:0.15
 
 ## [WARNING]: ensure YAML spec updated to current version number
 ## (elevate-apiserver-deployment.yaml)
