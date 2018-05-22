6b4f5b2c6cadf1c7296657d9d51bb5c28d8e070b
Tiffany <tzchen@uw.edu>
2018-05-21 20:38:01 -0700

    write resources handler

--- servers/elevate/handlers/resources.go
+++ servers/elevate/handlers/resources.go
@@ -1,37 +1,64 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/leedsjb/capstone2k18/servers/elevate/models/messages"
 )
 
 // Resource ...
-type Resource struct {
+type resourceRow struct {
 	ID        int    `json:"id"`
 	Link      string `json:"link"`
 	Name      string `json:"name"`
 	ImageLink string `json:"imageLink"`
 }
 
-var resources = []*Resource{
-	{
-		ID:        1,
-		Link:      "www.google.com",
-		Name:      "Google",
-		ImageLink: "http://www.stickpng.com/assets/images/580b57fcd9996e24bc43c51f.png",
-	},
-	{
-		ID:        2,
-		Link:      "www.facebook.com",
-		Name:      "Facebook",
-		ImageLink: "http://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19751.png",
-	},
-}
+// var resources = []*Resource{
+// 	{
+// 		ID:        1,
+// 		Link:      "www.google.com",
+// 		Name:      "Google",
+// 		ImageLink: "http://www.stickpng.com/assets/images/580b57fcd9996e24bc43c51f.png",
+// 	},
+// 	{
+// 		ID:        2,
+// 		Link:      "www.facebook.com",
+// 		Name:      "Facebook",
+// 		ImageLink: "http://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19751.png",
+// 	},
+// }
 
 // ResourcesHandler ...
-func ResourcesHandler(w http.ResponseWriter, r *http.Request) {
+func (ctx *HandlerContext) ResourcesHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		respond(w, resources)
+		resourceList := []*messages.Resource{}
+		resourceRows, err := ctx.GetAllResources()
+		if err != nil {
+			fmt.Printf("Couldn't get resources from the database: %v", err)
+			return
+		}
+		resourceRow := &resourceRow{}
+		for resourceRows.Next() {
+			err = resourceRows.Scan(
+				&resourceRow.ID,
+				&resourceRow.Link,
+				&resourceRow.Name,
+				&resourceRow.ImageLink,
+			)
+			if err != nil {
+				fmt.Printf("Error scanning resource row: %v", err)
+			}
+			resource := &messages.Resource{
+				ID:   resourceRow.ID,
+				Link: resourceRow.Link,
+				Name: resourceRow.Name,
+			}
+			resourceList = append(resourceList, resource)
+		}
+		respond(w, resourceList)
 	default:
 		http.Error(w, "Method must be GET", http.StatusMethodNotAllowed)
 		return
