b2093be52ab0368ad1b1d1cefcd7e2d1883a60f1
Tiffany <tzchen@uw.edu>
2018-05-22 09:39:59 -0700

    update deployment files for v0.12, working people handlers

--- servers/elevate/image-build-deploy.sh
+++ servers/elevate/image-build-deploy.sh
@@ -2,11 +2,11 @@
 
 GOOS=linux go build
 
-docker build -t us.gcr.io/airliftnw-uw/go-apiserver:0.11 .
+docker build -t us.gcr.io/airliftnw-uw/go-apiserver:0.12 .
 
 go clean
 
-docker push us.gcr.io/airliftnw-uw/go-apiserver:0.11
+docker push us.gcr.io/airliftnw-uw/go-apiserver:0.12
 
 ## Warning: ensure YAML spec updated to current version number
 
