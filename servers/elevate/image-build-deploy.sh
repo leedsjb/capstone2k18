609659ad1a17d22d50d9fedff8584f0da36fe0d5
Tiffany <tzchen@uw.edu>
2018-05-24 22:03:23 -0700

    pull most recent deployment files from test/pull-subscribe

--- servers/elevate/image-build-deploy.sh
+++ servers/elevate/image-build-deploy.sh
@@ -2,13 +2,14 @@
 
 GOOS=linux go build
 
-docker build -t us.gcr.io/airliftnw-uw/go-apiserver:0.10 .
+docker build -t us.gcr.io/airliftnw-uw/go-apiserver:0.19 .
 
 go clean
 
-docker push us.gcr.io/airliftnw-uw/go-apiserver:0.10
+docker push us.gcr.io/airliftnw-uw/go-apiserver:0.19
 
-## Warning: ensure YAML spec updated to current version number
+## [WARNING]: ensure YAML spec updated to current version number
+## (elevate-apiserver-deployment.yaml)
 
 kubectl apply -f deployment/elevate-apiserver-deployment.yaml --record
 kubectl rollout status deployment/go-api-server-deployment
