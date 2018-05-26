989f3b17a615725aab4be160e7d2a15101255ec5
Tiffany <tzchen@uw.edu>
2018-05-26 10:17:29 -0700

    update deployment files for v0.23, all json ID tags changed to id

--- servers/elevate/image-build-deploy.sh
+++ servers/elevate/image-build-deploy.sh
@@ -2,11 +2,11 @@
 
 GOOS=linux go build
 
-docker build -t us.gcr.io/airliftnw-uw/go-apiserver:0.22 .
+docker build -t us.gcr.io/airliftnw-uw/go-apiserver:0.23 .
 
 go clean
 
-docker push us.gcr.io/airliftnw-uw/go-apiserver:0.22
+docker push us.gcr.io/airliftnw-uw/go-apiserver:0.23
 
 ## [WARNING]: ensure YAML spec updated to current version number
 ## (elevate-apiserver-deployment.yaml)
