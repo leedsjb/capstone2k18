60781c260a8d16bd87ea230ecf09d9c93fd3fd2d
Tiffany <tzchen@uw.edu>
2018-05-26 12:06:34 -0700

    update deployment files for v0.24, add aircraft category filter

--- servers/elevate/image-build-deploy.sh
+++ servers/elevate/image-build-deploy.sh
@@ -2,11 +2,11 @@
 
 GOOS=linux go build
 
-docker build -t us.gcr.io/airliftnw-uw/go-apiserver:0.23 .
+docker build -t us.gcr.io/airliftnw-uw/go-apiserver:0.24 .
 
 go clean
 
-docker push us.gcr.io/airliftnw-uw/go-apiserver:0.23
+docker push us.gcr.io/airliftnw-uw/go-apiserver:0.24
 
 ## [WARNING]: ensure YAML spec updated to current version number
 ## (elevate-apiserver-deployment.yaml)
