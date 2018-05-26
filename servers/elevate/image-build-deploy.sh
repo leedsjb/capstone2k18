6f0dc4ea9722b78a1117f5abbe0f12d1a2251f98
Tiffany <tzchen@uw.edu>
2018-05-26 16:31:43 -0700

    update deployment files for v0.28, finished waypoint structure

--- servers/elevate/image-build-deploy.sh
+++ servers/elevate/image-build-deploy.sh
@@ -2,11 +2,11 @@
 
 GOOS=linux go build
 
-docker build -t us.gcr.io/airliftnw-uw/go-apiserver:0.27 .
+docker build -t us.gcr.io/airliftnw-uw/go-apiserver:0.28 .
 
 go clean
 
-docker push us.gcr.io/airliftnw-uw/go-apiserver:0.27
+docker push us.gcr.io/airliftnw-uw/go-apiserver:0.28
 
 ## [WARNING]: ensure YAML spec updated to current version number
 ## (elevate-apiserver-deployment.yaml)
