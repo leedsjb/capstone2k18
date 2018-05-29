af380dd92b2f28ae768187b58601a8d00378f229
Vincent M. van der Meulen <meulen@uw.edu>
2018-05-29 13:26:41 -0700

    Express shadows in terms of darkness #sauron

--- clients/web/src/theme.js
+++ clients/web/src/theme.js
@@ -23,10 +23,10 @@ const colors = {
 };
 
 const boxShadows = {
-    low: `0 2px 8px ${colors.gray5}`,
-    lowRight: `2px 2px 8px ${colors.gray5}`,
-    mid: `0 4px 12px ${colors.gray5}`,
-    high: `0 8px 16px ${colors.gray5}`
+    low: `0 2px 8px rgba(0, 0, 0, 0.08)`,
+    lowRight: `2px 2px 8px rgba(0, 0, 0, 0.08)`,
+    mid: `0 4px 12px rgba(0, 0, 0, 0.08)`,
+    high: `0 8px 16px rgba(0, 0, 0, 0.08)`
 };
 
 const breakpoints = [32, 48, 64, 80].map(n => n + "em");
