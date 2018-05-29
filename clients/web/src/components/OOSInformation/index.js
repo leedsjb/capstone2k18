f48d5abf8af7b48c7d56932355d2a1153ef12026
Vincent M. van der Meulen <meulen@uw.edu>
2018-05-29 16:22:38 -0700

    Start adding icons to OOS

--- clients/web/src/components/OOSInformation/index.js
+++ clients/web/src/components/OOSInformation/index.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { Flex } from "grid-styled";
 
 import Box from "../Box";
 import Span from "../Span";
+import Icon from "../Icon";
 
 const OOSInformation = ({ OOS }) => {
     return (
@@ -13,16 +15,20 @@ const OOSInformation = ({ OOS }) => {
                 </Box>
             ) : null}
             {OOS.remaining ? (
-                <Box mt={4}>
-                    <Span fontWeight="bold">Time Remaining: </Span>
-                    <Span>{OOS.remaining}</Span>
-                </Box>
+                <Flex mt={4} alignItems="center">
+                    <Flex mr={1} alignItems="center">
+                        <Icon glyph="hourglass" size={20} color="black2" />
+                    </Flex>
+                    <Span>{`Available in ${OOS.remaining}`} </Span>
+                </Flex>
             ) : null}
             {OOS.duration ? (
-                <Box mt={4}>
-                    <Span fontWeight="bold">OOS Duration: </Span>
-                    <Span>{OOS.duration}</Span>
-                </Box>
+                <Flex mt={4} alignItems="center">
+                    <Flex mr={1} alignItems="center">
+                        <Icon glyph="stopwatch" size={20} color="black2" />
+                    </Flex>
+                    <Span>{`${OOS.duration} elapsed`} </Span>
+                </Flex>
             ) : null}
         </div>
     );
