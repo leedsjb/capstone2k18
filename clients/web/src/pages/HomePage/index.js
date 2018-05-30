5ca58af357f5529139f172a3b2c3889ebd1ad20c
Vincent M. van der Meulen <meulen@uw.edu>
2018-05-29 22:25:29 -0700

    Replace images

--- clients/web/src/pages/HomePage/index.js
+++ clients/web/src/pages/HomePage/index.js
@@ -71,7 +71,7 @@ const HomePage = ({ theme: { colors, breakpoints } }) => {
                                     Critical Communication for Airborne Medical
                                     Missions
                                 </Heading>
-                                <Measure mt={4} mb={0}>
+                                <Measure mt={4} mb={0} mr={[12, 12, 0]}>
                                     Elevate is a mission-critical,
                                     high-availability application for Airlift
                                     Northwest emergency flight nurses and pilots
